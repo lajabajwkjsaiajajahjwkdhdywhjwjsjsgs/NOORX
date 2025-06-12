a25f3c0dbbe69ee2d9418ec4022485c479fb4ae8
lajabajwkjsaiajajahjwkdhdywhjwjsjsgs <terabaaphume120@gmail.com>
2025-06-12 13:00:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # ----------------------------------------------------------------
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", '30DxNexGenBots938b57') # youtube song api ke
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots7ed2dc') # youtube song api ke
 # -------------------------------------------------------------------
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
